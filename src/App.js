4381ea6e262d9b83e720d1fbedbee62757bebce8
wlt <pocoy87@outlook.com>
2023-01-08 20:53:41 -0600

    creacion de vista para crud
    creacion de vista para crud

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Home from './Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import ClientList from './ClientList';
-import ClientEdit from "./ClientEdit";
 
-class App extends Component {
-  render() {
-    return (
-        <Router>
-          <Switch>
-            <Route path='/' exact={true} component={Home}/>
-            <Route path='/clients' exact={true} component={ClientList}/>
-            <Route path='/clients/:id' component={ClientEdit}/>
-          </Switch>
-        </Router>
-    )
-  }
+import { Home } from './Home';
+import { Department } from './Department';
+import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App container">
+        <h3 className="d-flex justify-content-center m-3">
+          My React App
+        </h3>
+
+        <nav className="navbar navbar-expand-sm bg-light navbar-dark">
+          <ul className="navbar-nav">
+            <li className="nav-item m-1">
+              <NavLink className="btn btn-light btn-outline-primary" to="/home">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item m-1">
+              <NavLink className="btn btn-light btn-outline-primary" to="/department">
+                Department
+              </NavLink>
+            </li>
+            <li className="nav-item m-1">
+              <NavLink className="btn btn-light btn-outline-primary" to="/employee">
+                Employee
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <switch>
+          <Route path='/home' component={Home}></Route>
+          <Route path='/department' component={Department}></Route>
+          <Route path='/employee' component={Employee}></Route>
+        </switch>
+      </div>
+    </BrowserRouter>
+  );
 }
-
 export default App;
